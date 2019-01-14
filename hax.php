c1f05bcb21014175128189e11f5ed36c54933cf0
btopro <btopro@outlook.com>
2019-01-14 10:51:08 -0500

    updates to match webcomponents plugin

--- hax.php
+++ hax.php
@@ -40,16 +40,7 @@ class HAXPlugin extends Plugin {
       $offsetLeft = $this->config->get('plugins.hax.offset_left');
       // discover and autoload our components
       $assets = $this->grav['assets'];
-      // Webcomponents plugin doesn't include the polyfill for admin editing pages. Adding it here.
-      $assets->addJS('user/data/webcomponents/bower_components/webcomponentsjs/' . WebcomponentsPlugin::polyfillLibrary(), array('priority' => 1000, 'group' => 'head'));
-      $file = $this->getBaseURL() . 'bower_components/wysiwyg-hax/wysiwyg-hax.html';
-      $imports = $this->createHTMLImport($file) . "\n";
-      // build the inline import w/ bable helpers
-      $inline = '!function(e){var r=e.babelHelpers={};r.typeof="function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?function(e){return typeof e}:function(e){return e&&"function"==typeof Symbol&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e},r.classCallCheck=function(e,r){if(!(e instanceof r))throw new TypeError("Cannot call a class as a function")},r.createClass=function(){function e(e,r){for(var t=0;t<r.length;t++){var n=r[t];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(e,n.key,n)}}return function(r,t,n){return t&&e(r.prototype,t),n&&e(r,n),r}}(),r.defineEnumerableProperties=function(e,r){for(var t in r){var n=r[t];n.configurable=n.enumerable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(e,t,n)}return e},r.defaults=function(e,r){for(var t=Object.getOwnPropertyNames(r),n=0;n<t.length;n++){var o=t[n],i=Object.getOwnPropertyDescriptor(r,o);i&&i.configurable&&void 0===e[o]&&Object.defineProperty(e,o,i)}return e},r.defineProperty=function(e,r,t){return r in e?Object.defineProperty(e,r,{value:t,enumerable:!0,configurable:!0,writable:!0}):e[r]=t,e},r.extends=Object.assign||function(e){for(var r=1;r<arguments.length;r++){var t=arguments[r];for(var n in t)Object.prototype.hasOwnProperty.call(t,n)&&(e[n]=t[n])}return e},r.get=function e(r,t,n){null===r&&(r=Function.prototype);var o=Object.getOwnPropertyDescriptor(r,t);if(void 0===o){var i=Object.getPrototypeOf(r);return null===i?void 0:e(i,t,n)}if("value"in o)return o.value;var a=o.get;if(void 0!==a)return a.call(n)},r.inherits=function(e,r){if("function"!=typeof r&&null!==r)throw new TypeError("Super expression must either be null or a function, not "+typeof r);e.prototype=Object.create(r&&r.prototype,{constructor:{value:e,enumerable:!1,writable:!0,configurable:!0}}),r&&(Object.setPrototypeOf?Object.setPrototypeOf(e,r):e.__proto__=r)},r.instanceof=function(e,r){return null!=r&&"undefined"!=typeof Symbol&&r[Symbol.hasInstance]?r[Symbol.hasInstance](e):e instanceof r},r.newArrowCheck=function(e,r){if(e!==r)throw new TypeError("Cannot instantiate an arrow function")},r.objectDestructuringEmpty=function(e){if(null==e)throw new TypeError("Cannot destructure undefined")},r.objectWithoutProperties=function(e,r){var t={};for(var n in e)r.indexOf(n)>=0||Object.prototype.hasOwnProperty.call(e,n)&&(t[n]=e[n]);return t},r.possibleConstructorReturn=function(e,r){if(!e)throw new ReferenceError("this hasn\'t been initialised - super() hasn\'t been called");return!r||"object"!=typeof r&&"function"!=typeof r?e:r},r.set=function e(r,t,n,o){var i=Object.getOwnPropertyDescriptor(r,t);if(void 0===i){var a=Object.getPrototypeOf(r);null!==a&&e(a,t,n,o)}else if("value"in i&&i.writable)i.value=n;else{var u=i.set;void 0!==u&&u.call(o,n)}return n},r.slicedToArray=function(){function e(e,r){var t=[],n=!0,o=!1,i=void 0;try{for(var a,u=e[Symbol.iterator]();!(n=(a=u.next()).done)&&(t.push(a.value),!r||t.length!==r);n=!0);}catch(e){o=!0,i=e}finally{try{!n&&u.return&&u.return()}finally{if(o)throw i}}return t}return function(r,t){if(Array.isArray(r))return r;if(Symbol.iterator in Object(r))return e(r,t);throw new TypeError("Invalid attempt to destructure non-iterable instance")}}(),r.taggedTemplateLiteral=function(e,r){return Object.freeze(Object.defineProperties(e,{raw:{value:Object.freeze(r)}}))},r.temporalRef=function(e,r,t){if(e===t)throw new ReferenceError(r+" is not defined - temporal dead zone");return e},r.temporalUndefined={},r.toArray=function(e){return Array.isArray(e)?e:Array.from(e)},r.toConsumableArray=function(e){if(Array.isArray(e)){for(var r=0,t=Array(e.length);r<e.length;r++)t[r]=e[r];return t}return Array.from(e)}}("undefined"==typeof global?self:global)
-  </script>' . $imports . "<script>";
       $assets->addCSS('plugin://hax/hax.css', array('priority' => 103, 'group' => 'head'));
-       // add it into the document
-      $assets->addInlineJs($inline, array('priority' => 103, 'group' => 'head'));
       $location = '';
       $paths2 = $this->grav['uri'];
       $paths = $this->grav['uri']->paths();
@@ -106,42 +97,6 @@ class HAXPlugin extends Plugin {
    */
   public function getBaseURL() {
     return $this->grav['base_url'] . '/user/data/webcomponents/';
-  }
-  
-
-  /**
-   * Return the file system directory for forming paths on the front end.
-   * @return string  The base path to the user / webcomponents directory
-   */
-  public function webcomponentsDir() {
-    return getcwd() . '/user/data/webcomponents/';
-  }
-  /**
-   * Simple HTML Import render.
-   */
-  public function createHTMLImport($path, $rel = 'import') {
-    return '<link rel="' . $rel . '" href="' . $path . '">';
-  }
-
-  /**
-   * Render a webcomponent to the screen.
-   * @param  [type] $vars [description]
-   * @return [type]       [description]
-   */
-  public function renderWebcomponent($vars) {
-    return '<' . $vars['tag'] . ' ' . $this->webcomponentAttributes($vars['properties']) . '>' . "\n" . $vars['innerHTML'] . "\n" . '</' . $vars['tag'] . '>' . "\n";
-  }
-  /**
-   * Convert array into attributes for placement in an HTML tag.
-   * @param  array  $attributes array of attribute name => value pairs
-   * @return string             HTML name="value" output
-   */
-  protected function webcomponentAttributes(array $attributes = array()) {
-    foreach ($attributes as $attribute => &$data) {
-      $data = implode(' ', (array) $data);
-      $data = $attribute . '="' . htmlspecialchars($data, ENT_QUOTES, 'UTF-8') . '"';
-    }
-    return $attributes ? ' ' . implode(' ', $attributes) : '';
   }
   /**
    * returns an array of app store definitions based
@@ -1395,31 +1350,20 @@ class HAXPlugin extends Plugin {
     );
   }
 
-    /**
-     * Check if webcomponents are installed.
-     */
-    private function verifyWebcomponentsInstallation() {
+  /**
+   * Check if webcomponents are installed.
+   */
+  private function verifyWebcomponentsInstallation() {
 
-      $grav = new Grav();
-      $required_files = array(
-          $this->webcomponentsDir() . 'bower_components/webcomponentsjs/' . WebcomponentsPlugin::polyfillLibrary(),
-          $this->webcomponentsDir() . 'bower_components/wysiwyg-hax/wysiwyg-hax.html',
-      );
+    $grav = new Grav();
 
-      // Check if webcomponents plugin is installed and enabled.
-      if (!isset($grav::instance()['config']['plugins']['webcomponents']) || $grav::instance()['config']['plugins']['webcomponents']['enabled'] == false){
-        $message = "Webcomponents plugin is not installed/ enabled.";
-        AtoolsPlugin::disablePlugin($message, 'error', 'hax');
-        return false;
-      }
-
-      foreach ($required_files as $file) {
-          if (!file_exists($file)) {
-              $message = 'Missing Hax dependency file at ' . $file;
-              AtoolsPlugin::disablePlugin($message, 'error', 'hax');
-              return false;
-          }
-      }
-      return true;
+    // Check if webcomponents plugin is installed and enabled.
+    if (!isset($grav::instance()['config']['plugins']['webcomponents']) || $grav::instance()['config']['plugins']['webcomponents']['enabled'] == false){
+      $message = "Webcomponents plugin is not installed/ enabled.";
+      AtoolsPlugin::disablePlugin($message, 'error', 'hax');
+      return false;
     }
+
+    return true;
+  }
 }
